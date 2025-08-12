a1a8c0123489536346b2b2801a13f16648256854
Ashley Sue <ashley.sue0605@gmail.com>
2025-08-12 12:55:53 -0700

    Add restaurantPage

---
+++ LocalLens/restaurantPage.swift
@@ -0,0 +1,18 @@
+//
+//  restaurantPage.swift
+//  LocalLens
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+struct restaurantPage: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    restaurantPage()
+}
---
+++ restaurantPage.swift
@@ -0,0 +1,8 @@
+//
+//  restaurantPage.swift
+//  LocalLens
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import Foundation
