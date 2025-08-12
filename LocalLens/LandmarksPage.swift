32da67278e0be9863b3083c2dfd69f005abc5dea
Olivia Chen <baomien@gmail.com>
2025-08-12 12:53:17 -0700

    Added LandmarksPage - Olivia

---
+++ LocalLens/LandmarksPage.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksPage.swift
+//  LocalLens
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+struct LandmarksPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarksPage()
+}
