4fff5f3bf3f836662be504ccb0a3a87d3f0de9fc
Olivia Chen <baomien@gmail.com>
2025-08-12 15:38:34 -0700

    Added vertical scroll bar and sized all images

--- LocalLens/LandmarksPage.swift
+++ LocalLens/LandmarksPage.swift
@@ -9,76 +9,77 @@ import SwiftUI
 
 struct LandmarksPage: View {
     var body: some View {
-        ZStack{
-            Color("GreenYellow")
-                .ignoresSafeArea()
-                .padding(.top, 50.0)
+            ZStack{
+                Color("GreenYellow")
+                    .ignoresSafeArea()
+                    .padding(.top, 50.0)
                 
-            VStack{
-                Text("Landmarks")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                    .padding(.bottom, 10.0)
-                
-                HStack{
-                    Button("Touristy") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
-                    .buttonStyle(.borderedProminent)
-                    .tint(.asparagus)
-                    .padding()
-
-                    Button("Local") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
-                    .buttonStyle(.borderedProminent)
-                    .tint(.asparagus)
-                    .padding()
-
-                    Button("Waterfront") {
-                            
-                    }
-                    .buttonStyle(.borderedProminent)
-                    .tint(.asparagus)
-                    .padding()
-                }//HStack end
-            
-                HStack{
-                    Image("GGBridge")
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(10)
-                        .frame(width: 200, height: 100)
-                        .clipped()
-                        .shadow(color: .black.opacity(0.5), radius: 10, x: 5, y: 4)
-                    Image("FishermansWharf")
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(10)
-                        .frame(width: 200, height: 100)
-                        .clipped()
-                        .shadow(color: .black.opacity(0.5), radius: 10, x: 5, y: 4)
-                      
-                }//Hstack end
-                .padding()
-
-                HStack{
-                    Image("PaintedLadies")
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(10)
-                        .frame(width: 200, height: 100)
-                        .shadow(color: .black.opacity(0.5), radius: 10, x: 5, y: 4)
-                    Image("CoitTower")
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(10)
-                        .frame(width: 200, height: 100)
-                        .shadow(color: .black.opacity(0.7), radius: 10, x: 5, y: 4)
+                ScrollView(.vertical){//Click and hold to scroll
+                    VStack {
+                        Text("Landmarks")
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            .padding(.top, 75.0)
                         
-                }//hstack end
-            }//VStack end
-        }//ZStack
+                        
+                        HStack {
+                            Button("Touristy") {
+                                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                            }
+                            .buttonStyle(.borderedProminent)
+                            .tint(.asparagus)
+                            .padding()
+                            
+                            Button("Local") {
+                                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                            }
+                            .buttonStyle(.borderedProminent)
+                            .tint(.asparagus)
+                            .padding()
+                            
+                            Button("Waterfront") {
+                                
+                            }
+                            .buttonStyle(.borderedProminent)
+                            .tint(.asparagus)
+                            .padding()
+                        }//HStack end
+                        
+                        Image("GGBridge")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(10)
+                            .frame(width: 300, height: 200)
+                            .shadow(color: .black.opacity(0.5), radius: 10, x: 5, y: 4)
+                            .padding()
+                        Image("FishermansWharf")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(10)
+                            .frame(width: 300, height: 175)
+                            .shadow(color: .black.opacity(0.5), radius: 10, x: 5, y: 4)
+                            .padding()
+                        
+                        Image("PaintedLadies")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(10)
+                            .frame(width: 300, height: 200)
+                            .shadow(color: .black.opacity(0.5), radius: 10, x: 5, y: 4)
+                            .padding()
+                        
+                        Image("CoitTower")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(10)
+                            .frame(width: 300, height: 200)
+                            .shadow(color: .black.opacity(0.7), radius: 10, x: 5, y: 4)
+                            .padding()
+                        
+                        
+                    }//VStack end
+                }
+            }//ZStack
     }//body end
 }//struct end,
 
