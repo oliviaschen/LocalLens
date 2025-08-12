a1955abeff219ec5bcb765349ac87969c001863f
Aasiya Choudhry <aasiya.s.choudhry@gmail.com>
2025-08-12 12:53:28 -0700

    Added homepage

---
+++ LocalLens/Homepage.swift
@@ -0,0 +1,18 @@
+//
+//  Homepage.swift
+//  LocalLens
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+struct Homepage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Homepage()
+}
