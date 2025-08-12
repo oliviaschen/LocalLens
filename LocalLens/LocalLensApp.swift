8c308400498acf3c4eb9cedbf5984af6023bd9d1
Olivia Chen <baomien@gmail.com>
2025-08-12 11:12:29 -0700

    Initial Commit

---
+++ LocalLens/LocalLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalLensApp.swift
+//  LocalLens
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
