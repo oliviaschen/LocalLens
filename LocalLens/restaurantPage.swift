c417d9478a59abb153df1ceb71ad0f4dfdbd9daf
Ashley Sue <ashley.sue0605@gmail.com>
2025-08-12 15:47:14 -0700

    Added Scroll, labels,  first image

--- LocalLens/restaurantPage.swift
+++ LocalLens/restaurantPage.swift
@@ -12,39 +12,85 @@ struct restaurantPage: View {
         ZStack{
             Color(.greenYellow)
                 .ignoresSafeArea()
-            VStack(alignment: .leading, spacing: 20.0) {
-                HStack{
-                    
-                    Image(.threeLinesMenuSymbol)
-                        .renderingMode(.original)
-                        .resizable()
-                        .frame(width:40, height:40)
-                        .aspectRatio(contentMode: .fill)
-                        .padding(30)
-                    
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0.0) {
                     HStack{
                         
-                        Text("Restaurants")
-                            .font(.largeTitle)
-                            .fontWeight(.semibold)
-                            .foregroundColor(Color("caribbeanCurrent"))
-                            .padding()
+                        Image(.threeLinesMenuSymbol)
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width:40, height:40)
+                            .aspectRatio(contentMode: .fill)
+                            .padding(30)
+                        
+                        HStack{
+                            
+                            Text("Restaurants")
+                                .font(.largeTitle)
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color("caribbeanCurrent"))
+                                .padding()
+                        }//end HStack
+                        
                     }//end HStack
+                    Text("Popular")
+                        .fontWeight(.semibold)
+                        .padding()
+                        .font(.title)
+                        .foregroundColor(Color("caribbeanCurrent"))
+                    RoundedRectangle(cornerRadius: 20).foregroundColor(.asparugusGreen)
+                        .frame(width:375, height:185)
+                        .padding()
+                    
+                    Text("Cafes & Drinks")
+                        .fontWeight(.semibold)
+                        .padding()
+                        .font(.title)
+                        .foregroundColor(Color("caribbeanCurrent"))
+                    ZStack{
+                        
+                        RoundedRectangle(cornerRadius: 20).foregroundColor(.asparugusGreen)
+                            .frame(width:375, height:185)
+                            .padding()
+                        VStack {
+                            HStack {
+                                Image(.goldenIsland)
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 100, height:100)
+                                    .aspectRatio(contentMode: .fill)
+                                    .cornerRadius(15)
+                                    .padding()
 
-                }//end HStack
-                RoundedRectangle(cornerRadius: 20).foregroundColor(.asparugusGreen)
-                    .frame(width:375, height:185)
-                    .padding()
-        
-                RoundedRectangle(cornerRadius: 20).foregroundColor(.asparugusGreen)
-                    .frame(width:375, height:185)
-                    .padding()
-                
-                RoundedRectangle(cornerRadius: 20).foregroundColor(.asparugusGreen)
-                    .frame(width:375, height:185)
-                    .padding()
-            }//end VStack
-        }//end ZStack
+                                Spacer()
+                            }//end Hstack
+                            Spacer()
+                        }//end VStack
+
+                    }//end ZStack
+                    
+                    
+                    Text("Inexpensive")
+                        .fontWeight(.semibold)
+                        .padding()
+                        .font(.title)
+                        .foregroundColor(Color("caribbeanCurrent"))
+                    RoundedRectangle(cornerRadius: 20).foregroundColor(.asparugusGreen)
+                        .frame(width:375, height:185)
+                        .padding()
+                    
+                    Text("Bakeries")
+                        .fontWeight(.semibold)
+                        .padding()
+                        .font(.title)
+                        .foregroundColor(Color("caribbeanCurrent"))
+                    RoundedRectangle(cornerRadius: 20).foregroundColor(.asparugusGreen)
+                        .frame(width:375, height:185)
+                        .padding()
+                }//end VStack
+            }//end ScrollView
+            }//end Zstack
+            
     }//end body
 }//end struck
 
