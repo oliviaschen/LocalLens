5617c9ffcb1b1c71ad10232da7bd0c9e05281b51
Kalani Davis <kalani@jtde.com>
2025-08-13 10:14:41 -0700

    title text DONE

--- LocalLens/ActivitiesPage.swift
+++ LocalLens/ActivitiesPage.swift
@@ -15,9 +15,14 @@ struct ActivitiesPage: View {
             //note: cornerRadius must go before padding so that padding applies to the view AND the corners
             
             VStack(spacing:200){
-                HStack {
-                    Text("Conservatory of Flowers")
-                }
+                // SECT 1
+                VStack {
+                    HStack {
+                        Text("Conservatory of Flowers")
+                        // padding centers the title to the top left of the section. Note that I decrease padding from the top rather than add padding to the bottom because of the spacing between sections (my preference)
+                            .padding(EdgeInsets(top:-80, leading:-140, bottom:0, trailing:0))
+                    }
+                }//end SECT 1
                 .background(Rectangle().fill(Color.greenYellow).cornerRadius(30).padding().frame(width:400, height:230))
             
                 HStack {
