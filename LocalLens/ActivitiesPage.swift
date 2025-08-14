97470f921e77dab663aa6e7497fd202983f98ea8
sleepyturtlee <kalani@jtde.com>
2025-08-14 09:17:43 -0700

    developed UI (home work)

--- LocalLens/ActivitiesPage.swift
+++ LocalLens/ActivitiesPage.swift
@@ -3,9 +3,9 @@ import SwiftData
 
 struct ActivitiesPage: View {
     @State private var Activities:[Activity] = [
-        Activity(name: "Exploratorium", imageName: "Exploratorium", desc: "One of the top, most interactive science museums in the U.S."),
-        Activity(name: "Conservatory Of Flowers", imageName:"ConservatoryOfFlowers", desc: "A beautiful garden with a wide variety of plants from all over the world."),
-        Activity(name: "Japantown", imageName:"Japantown", desc: "A great shopping center, full of bustling shops and Japanese culture.")
+        Activity(name: "Exploratorium", imageName: "Exploratorium", desc: "One of the top, most interactive science museums in the U.S.", cost:2, district: "Embaracdero"),
+        Activity(name: "Conservatory Of Flowers", imageName:"ConservatoryOfFlowers", desc: "A beautiful garden with a wide variety of plants from all over the world.", cost:2, district: "Golden Gate Park"),
+        Activity(name: "Japantown", imageName:"Japantown", desc: "A great shopping center, full of bustling shops and Japanese culture.", cost:2, district: "Japantown")
     ]
     var body: some View {
         Text("Activities")
@@ -22,9 +22,11 @@ struct ActivitiesPage: View {
                         //Image(s)
                         HStack {
                             Image(Activities[index].imageName)
-                                .resizable(capInsets: EdgeInsets(), resizingMode: .stretch)
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width:0, height: 90.0)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 100, height: 100)
+                                .clipped()
+                                .cornerRadius(10)
                         }
                         .padding(EdgeInsets(top:-100, leading:0, bottom:0, trailing:0))
                         //brings the image up and to the left
@@ -38,27 +40,28 @@ struct ActivitiesPage: View {
                             Spacer()
                         }//end Hstack
                         // makes the text go down
-                        .padding(EdgeInsets(top:40, leading:0, bottom:0, trailing:0))
+                        .padding(EdgeInsets(top:40, leading:-2, bottom:0, trailing:0))
                         // makes the text a little away from the edge.
                         .padding(.horizontal)
                         
                         //Cost and District
                         HStack {
-                            Text("District reean")
+                            Text(Activities[index].district)
                                 .font(.footnote)
-                            Text("$$$$")
+                            Text("$$")
                             Spacer()
                         }
                         .padding(.horizontal)
                         .padding(EdgeInsets(top:100, leading:0, bottom:0, trailing:0))
-//                        .font(.caption)
                         
                         
                         //Description
-                        Text(Activities[index].desc)
-                            .padding(EdgeInsets(top:190, leading:0, bottom:0, trailing:0))
-                            .padding(.horizontal)
-                            
+                        HStack {
+                            Text(Activities[index].desc)
+                                .font(.footnote)
+                        }//end HStack
+                        .padding(.horizontal)
+                        .padding(EdgeInsets(top:170, leading:0, bottom:0, trailing:0))
                     }//end ZStack
                     .padding(.horizontal)
                     .frame(width:400, height:250)//makes boxes
@@ -68,6 +71,7 @@ struct ActivitiesPage: View {
         }//end ScrollView
             .background(Color.asparagus)
 //        .background(Color.gray.opacity(0.1)) //to view boundaries
+
     }
 }
 
--- LocalLens/Activity.swift
+++ LocalLens/Activity.swift
@@ -5,15 +5,19 @@ class Activity {
     var name:String
     //image name
     var imageName:String
+    var imageName2:String
+    var imageName3:String
     // description of the activity
     var desc:String
     // cost will be represented with # of dollar signs: Free -> $-$$$$, a range of 0-5
-    var cost:Double
+    var cost:Int
     var district:String
     
-    init(name:String="Activity Name", imageName:String="", desc:String="desc. here", cost:Double=0, district:String="district here") {
+    init(name:String="Activity Name", imageName:String="", desc:String="desc. here", cost:Int=0, district:String="district here") {
         self.name = name
         self.imageName = imageName
+        self.imageName2 = imageName + "2"
+        self.imageName3 = imageName + "3"
         self.desc = desc
         self.cost = cost
         self.district = district
