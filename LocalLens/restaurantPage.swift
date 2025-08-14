f3992a85d9c9e7788c6b80539039f7915ed9ce9d
Ashley Sue <ashley.sue0605@gmail.com>
2025-08-14 11:12:44 -0700

    Added new places

--- LocalLens/restaurantPage.swift
+++ LocalLens/restaurantPage.swift
@@ -37,7 +37,8 @@ struct restaurantPage: View {
                         Image(.threeLinesMenuSymbol)
                             .renderingMode(.original)
                             .resizable()
-                            .frame(width:40, height:40)
+                            .frame(width:35, height:35)
+
                             .aspectRatio(contentMode: .fill)
                             .padding(30)
                         
@@ -103,6 +104,49 @@ struct restaurantPage: View {
                         
                     }//end ZStack
                     
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20).foregroundColor(.asparugusGreen)
+                            .frame(width:375, height:185)
+                            .padding()
+                        
+                        VStack{
+                            HStack(alignment: .top, spacing: 10.0){
+                                Spacer()
+                                HeartButton()
+                                    .position(x:250, y: 35)
+                                
+                                Image(.chatGPTImageAug132025122733PM)
+                                    .resizable()
+                                    .foregroundColor(Color("caribbeanCurrent"))
+                                    .frame(width: 35, height:25)
+                                    .padding(25)
+                            }//end HStack
+                            Spacer(minLength: 20.0)
+                        }//end VStack
+                        
+                        VStack {
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                Image(.brendasFrenchSoulFood)
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 150, height:150)
+                                    .aspectRatio(contentMode: .fill)
+                                    .cornerRadius(15)
+                                    .padding()
+
+                                Text("Brenda's French Soul Food: French, Creole, and Southern cuisine - 652 Polk St, San Francisco")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(Color("GreenYellow"))
+                                    .padding(20)
+                                Spacer()
+                            }//end HStack
+                            Spacer()
+                        }//end VStack
+                    }//end ZStack
+                    
                     Text("Cafes & Drinks")
                         .fontWeight(.semibold)
                         .padding()
@@ -139,6 +183,47 @@ struct restaurantPage: View {
                                     .cornerRadius(15)
                                     .padding()
                                 Text("Golden Island Cafe: Chinese dessert and drink cafe - 1300 Noriega St, San Francisco")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(Color("GreenYellow"))
+                                    .padding()
+                                Spacer()
+                            }//end HStack
+                            Spacer()
+                        }//end VStack
+                    }//end ZStack
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20).foregroundColor(.asparugusGreen)
+                            .frame(width:375, height:185)
+                            .padding()
+                        VStack{
+                            HStack(alignment: .top, spacing: 10.0){
+                                Spacer()
+                                HeartButton()
+                                    .position(x:250, y: 35)
+                                
+                                Image(.chatGPTImageAug132025124759PM)
+                                    .resizable()
+                                    .foregroundColor(Color("caribbeanCurrent"))
+                                    .frame(width: 20, height:25)
+                                    .padding(25)
+                            }//end HStack
+                            Spacer(minLength: 20.0)
+                        }//end VStack
+                        
+                        VStack {
+                          Spacer()
+                            HStack {
+                                Spacer()
+                                Image(.sanaCafe)
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 150, height:150)
+                                    .aspectRatio(contentMode: .fill)
+                                    .cornerRadius(15)
+                                    .padding()
+                                Text("Sana'a Cafe: Yemeni styled cafe with pastries - 199 New Montgomery St, San Francisco")
                                     .font(.headline)
                                     .fontWeight(.heavy)
                                     .foregroundColor(Color("GreenYellow"))
@@ -200,6 +285,47 @@ struct restaurantPage: View {
                         
                     }//end ZStack
                     
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20).foregroundColor(.asparugusGreen)
+                            .frame(width:375, height:185)
+                            .padding()
+                        VStack{
+                            HStack(alignment: .top, spacing: 10.0){
+                                Spacer()
+                                HeartButton()
+                                    .position(x:250, y: 35)
+                                
+                                Image(.chatGPTImageAug132025122733PM)
+                                    .resizable()
+                                    .foregroundColor(Color("caribbeanCurrent"))
+                                    .frame(width: 35, height:25)
+                                    .padding(25)
+                            }//end HStack
+                            Spacer(minLength: 20.0)
+                        }//end VStack
+                        
+                        VStack {
+                          Spacer()
+                            HStack {
+                                Spacer()
+                                Image(.pacificCatch)
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 150, height:150)
+                                    .aspectRatio(contentMode: .fill)
+                                    .cornerRadius(15)
+                                    .padding()
+                                Text("Pacific Catch: Seafood inspired by Pacific Rim - 1200 9th Ave, San Francisco")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(Color("GreenYellow"))
+                                    .padding()
+                                Spacer()
+                            }//end HStack
+                            Spacer()
+                        }//end VStack
+                    }//end ZStack
+                    
                     Text("Bakeries")
                         .fontWeight(.semibold)
                         .padding()
@@ -247,6 +373,46 @@ struct restaurantPage: View {
                         }//end VStack
                        
                         
+                    }//end ZStack
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20).foregroundColor(.asparugusGreen)
+                            .frame(width:375, height:185)
+                            .padding()
+                        VStack{
+                            HStack(alignment: .top, spacing: 10.0){
+                                Spacer()
+                                HeartButton()
+                                    .position(x:250, y: 35)
+                                
+                                Image(.chatGPTImageAug132025122733PM)
+                                    .resizable()
+                                    .foregroundColor(Color("caribbeanCurrent"))
+                                    .frame(width: 35, height:25)
+                                    .padding(25)
+                            }//end HStack
+                            Spacer(minLength: 20.0)
+                        }//end VStack
+                        
+                        VStack {
+                          Spacer()
+                            HStack {
+                                Spacer()
+                                Image(.noeValleyBakery)
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 150, height:150)
+                                    .aspectRatio(contentMode: .fill)
+                                    .cornerRadius(15)
+                                    .padding()
+                                Text("Noe Valley Bakery: Sweets, breakfast items, and cookie dough for home baking -8  W Portal Ave, San Francisco ")
+                                    .font(.headline)
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(Color("GreenYellow"))
+                                    .padding()
+                                Spacer()
+                            }//end HStack
+                            Spacer()
+                        }//end VStack
                     }//end ZStack
                 }//end VStack
             }//end ScrollView
